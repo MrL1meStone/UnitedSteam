f2b1334f093cb9376e6e5c9b38916ea93864bd1d
sgooi <vbv0706@gmail.com>
2025-08-04 17:38:57 +0400

    new database

--- database.py
+++ database.py
@@ -2,65 +2,89 @@ import sqlite3
 connection = sqlite3.connect('users.db')
 cursor = connection.cursor()
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS Members (
 id INTEGER PRIMARY KEY,
 nick TEXT NOT NULL,
-age INTEGER,
-admin INTEGER,
-member INTEGER
+age INTEGER
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Requests (
+id INTEGER PRIMARY KEY,
+nick TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Admins (
+id INTEGER PRIMARY KEY,
+nick TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Bans (
+id INTEGER PRIMARY KEY,
+nick TEXT NOT NULL,
+age INTEGER
 )
 ''')
 
 
 def new_user(user_id,nick,age) -> None | int:
-    cursor.execute('SELECT id FROM Users Where id = ?', (user_id,))
-    if not cursor.fetchall():
-        cursor.execute('INSERT INTO Users (id,nick,age,admin,member) VALUES (?, ?, ?,0,0)', (user_id, nick, age))
-        connection.commit()
-        return None
-    else:
-        return 0
+	cursor.execute('INSERT INTO Requests (id,nick,age) VALUES (?, ?, ?)', (user_id, nick, age))
+	connection.commit()
 
-def return_database() -> list:
-    cursor.execute('SELECT * FROM Users')
-    return cursor.fetchall()
-
-def return_members()-> list:
-    cursor.execute('SELECT id,nick FROM Users WHERE member = 1')
-    return cursor.fetchall()
-
-def return_requests() -> list:
-    cursor.execute('SELECT id,nick,age FROM Users WHERE member = 0')
-    return cursor.fetchall()
-
-def return_admins() -> list:
-    cursor.execute('SELECT id FROM Users WHERE admin = 1')
-    return [i[0] for i in cursor.fetchall()]
+def return_from(return_type)-> list:
+	cursor.execute(f"SELECT * FROM {return_type}")
+	return cursor.fetchall()
 
 def make_admin(user_id) -> None:
-    cursor.execute('UPDATE Users SET admin = 1 WHERE id = ? AND member = 1', (user_id,))
-    connection.commit()
+	cursor.execute(f"SELECT id,nick,age FROM Members WHERE id=?",(user_id,))
+	user_id,nick,age=cursor.fetchone()
+	cursor.execute('INSERT INTO Admins id,nick,age VALUES (?, ?, ?)', (user_id,nick,age))
+	connection.commit()
 
 def is_admin(user_id) -> bool:
-    cursor.execute('SELECT admin FROM Users Where id = ?',(user_id,))
-    admin : tuple[int] | None = cursor.fetchone()
-    if not admin: return False
-    return bool(admin[0])
+	cursor.execute("SELECT * FROM Admins WHERE id=?",(user_id,))
+	return bool(cursor.fetchone())
 
 def make_member(user_id) -> None:
-    cursor.execute('UPDATE Users SET member = 1 WHERE id = ? AND member = 0', (user_id,))
-    connection.commit()
+	cursor.execute(f"SELECT id,nick,age FROM Requests WHERE id=?", (user_id,))
+	user_id, nick, age = cursor.fetchone()
+	cursor.execute('INSERT INTO Members id,nick,age VALUES (?, ?, ?)', (user_id, nick, age))
+	cursor.execute('DELETE FROM Requests WHERE id = ?', (user_id,))
+	connection.commit()
 
 def is_member(user_id) -> bool:
-    cursor.execute('SELECT member FROM Users Where id = ?', (user_id,))
-    admin: tuple[int] | None = cursor.fetchone()
-    if not admin: return False
-    return bool(admin[0])
+	cursor.execute('SELECT * FROM Members Where id = ?', (user_id,))
+	return bool(cursor.fetchone())
 
 def remove_member(user_id) -> None:
-    cursor.execute('DELETE FROM Users WHERE id = ?', (user_id,))
-    connection.commit()
+	cursor.execute('DELETE FROM Users WHERE id = ?', (user_id,))
+	cursor.execute('DELETE FROM Admins WHERE id = ?', (user_id,))
+	cursor.execute('DELETE FROM Requests WHERE id = ?', (user_id,))
+	connection.commit()
 
-def remove_admin(user_id) -> None:
-    cursor.execute('UPDATE Users SET admin = 0 WHERE id = ?', (user_id,))
-    connection.commit()
+def ban(user_id) -> None:
+	if not is_member(user_id):
+		cursor.execute(f"SELECT nick,age FROM Requests WHERE id=?", (user_id,))
+		nick,age=cursor.fetchone()
+	else:
+		cursor.execute(f"SELECT nick,age FROM Members WHERE id=?", (user_id,))
+		nick, age = cursor.fetchone()
+
+	cursor.execute('INSERT INTO Bans id,nick,age VALUES (?, ?, ?)', (user_id, nick, age))
+	remove_member(user_id)
+	connection.commit()
+
+def is_banned(user_id) -> bool:
+	cursor.execute(f"SELECT * FROM Bans WHERE id=?", (user_id,))
+	return bool(cursor.fetchone())
+
+def unban(user_id) -> None:
+	cursor.execute('DELETE FROM Bans WHERE id = ?', (user_id,))
+	connection.commit()
